060386ee482d50dd9bc5f53fda63e102e54bb7d7
Jonathan Ah-Nee <jonahnee@gmail.com>
2015-12-29 19:51:49 -1000

    GET switch case set up, starting POST

--- server.js
+++ server.js
@@ -7,32 +7,43 @@ var server = http.createServer(function requestHandler(req, res) {
     console.log(data.toString());
   });
 
-  // Sets resource to index.html if / requested.
-  // If not /, sets to requested page.
-  req.on('end', function (data) {
-    console.log(req.url);
-    var resource = 'public/';
-    if (req.url === '/') {
-      resource += 'index.html';
-    } else {
-      resource += req.url;
-    }
-
-    //
-    fs.readFile(resource, function (err, data) {
-      // If err, loads 404 page
-      if (err) {
-        console.log('Page cannot be found');
-        res.statusCode = 404;
-        res.statusMessage = "Page cannot be found...";
-        return fs.readFile('public/404.html', function (err, rawbuffer) {
-          return res.end(rawbuffer);
-        });
+  switch (req.method.toUpperCase()) {
+    // GET Case
+    case 'GET':
+    console.log('Processing GET request');
+    req.on('end', function (data) {
+      // Console logs requested page
+      console.log(req.url);
+      var resource = 'public/';
+      if (req.url === '/') {
+        resource += 'index.html';
+      } else {
+        resource += req.url;
       }
-      // If no err, loads requested page
-      return res.end(data.toString());
+
+      fs.readFile(resource, function (err, data) {
+        // If err, loads 404 page
+        if (err) {
+          console.log('Page cannot be found');
+          res.statusCode = 404;
+          res.statusMessage = "Page cannot be found...";
+          return fs.readFile('public/404.html', function (err, rawbuffer) {
+            return res.end(rawbuffer);
+          });
+        }
+        // If no err, loads requested page
+        return res.end(data.toString());
+      });
     });
-  });
+    break;
+
+    // POST Case
+    case 'POST':
+    console.log('Processing POST request');
+
+
+    break;
+  }
 });
 
 // Listens for the server
